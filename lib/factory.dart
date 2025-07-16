307949df167d02af7b753fc884f80393fd47e71d
Yahyazawadi <yahyazawadi278@gmail.com>
2025-07-16 16:18:26 +0300

    added factory

--- lib/factory.dart
+++ lib/factory.dart
@@ -4,12 +4,9 @@ import 'package:online_store/products/electronics/headphone.dart';
 import 'package:online_store/products/electronics/keyboard.dart';
 import 'package:online_store/products/product.dart';
 
-/// A function that creates a [Product] instance interactively.
 typedef ProductCreator = Product Function();
 
-/// A factory that builds different [Product] subclasses based on a type key.
 class ProductFactory {
-  /// Registry mapping lowercase product type names to their creators.
   static final Map<String, ProductCreator> _creators = {
     'headphone': () => Headphone.create(),
     'keyboard': () => Keyboard.create(),
@@ -17,9 +14,6 @@ class ProductFactory {
     'shoe': () => Shoe.create(),
   };
 
-  /// Creates a product of the given [type], using the registered creator.
-  ///
-  /// Throws [ArgumentError] if the type is not supported.
   static Product createProduct(String type) {
     final creator = _creators[type.toLowerCase()];
     if (creator == null) {
@@ -28,6 +22,5 @@ class ProductFactory {
     return creator();
   }
 
-  /// Returns the list of supported product types.
   static List<String> availableTypes() => _creators.keys.toList();
 }

a3b383e21cefed2c73c443cb46cf920eb81755a9
Yahyazawadi <yahyazawadi278@gmail.com>
2025-07-16 10:31:23 +0300

    added models

---
+++ lib/models/product.dart
@@ -0,0 +1,48 @@
+abstract class Product {
+  String name;
+  double price;
+  int quantity;
+  String category;
+
+  Product({
+    required this.name,
+    required this.price,
+    required this.quantity,
+    required this.category,
+  }) {
+    if (price < 0) {
+      throw ArgumentError('Price cannot be negative');
+    }
+    if (quantity < 0) {
+      throw ArgumentError('Quantity cannot be negative');
+    }
+  }
+
+  double discount();
+  double finalPrice() {
+    return price - discount();
+  }
+
+  decreaseQuantity(int amount) {
+    if (amount < 0) {
+      throw ArgumentError('Amount to decrease cannot be negative');
+    }
+    if (amount > quantity) {
+      throw ArgumentError(
+        'Cannot decrease quantity by more than available stock',
+      );
+    }
+    quantity -= amount;
+    print("Quantity decreased by $amount. New quantity: $quantity");
+  }
+
+  increaseQuantity(int amount) {
+    if (amount < 0) {
+      throw ArgumentError('Amount to increase cannot be negative');
+    }
+    quantity += amount;
+    print('$name quantity increased by $amount. New quantity: $quantity');
+  }
+
+  String listingInfo();
+}
